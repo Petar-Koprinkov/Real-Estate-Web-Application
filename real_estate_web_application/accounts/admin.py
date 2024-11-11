42ab135a48aa556b635e7fde7626ffb8ac568ec7
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-11 15:39:24 +0200

    feat: Created Custom Admin

--- real_estate_web_application/accounts/admin.py
+++ real_estate_web_application/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from real_estate_web_application.accounts.forms import CustomUserChangeForm, CustomUserCreationForm
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class CustomUserModelAdmin(UserAdmin):
+    model = UserModel
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ("pk", "username", "is_staff", "is_superuser")
+    ordering = ("pk",)
+
+    fieldsets = (
+        ("Credentials", {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
--- real_estate_web_application/accounts/forms.py
+++ real_estate_web_application/accounts/forms.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-userModel = get_user_model()
+UserModel = get_user_model()
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = userModel
+        model = UserModel
         fields = ('username',)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = '__all__'
+
 
