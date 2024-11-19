9f243d3ff1bdff31f9ccfb8dff3e414d07d844c0
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-19 23:02:24 +0200

    fix: Addedd user type to be edited

--- real_estate_web_application/accounts/forms.py
+++ real_estate_web_application/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+from real_estate_web_application.accounts.choices import UserTypeChoice
 from real_estate_web_application.accounts.models import Profile
 
 UserModel = get_user_model()
@@ -19,6 +20,30 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class EditProfileForm(forms.ModelForm):
+    user_type = forms.ChoiceField(
+        choices=UserTypeChoice.choices,
+        required=True,
+        label="User Type",
+    )
+
     class Meta:
         model = Profile
         exclude = ['user']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.user:
+            self.fields['user_type'].initial = self.instance.user.user_type
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        if commit:
+            profile.save()
+
+        if 'user_type' in self.cleaned_data:
+            user = profile.user
+            user.user_type = self.cleaned_data['user_type']
+            if commit:
+                user.save()
+
+        return profile
