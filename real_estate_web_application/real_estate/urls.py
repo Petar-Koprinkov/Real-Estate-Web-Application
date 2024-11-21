2bd267a67bfb60538705c5a0f8e494cdc4c92799
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-21 16:34:05 +0200

    feat: Added Dashboard and pagination to it, but it is not finished yet

--- real_estate_web_application/real_estate/urls.py
+++ real_estate_web_application/real_estate/urls.py
@@ -4,4 +4,5 @@ from real_estate_web_application.real_estate import views
 
 urlpatterns = [
     path('create_location', views.CreateLocationView.as_view(), name='create-location'),
+    path('properties/', views.PropertyListView.as_view(), name='properties'),
 ]
--- real_estate_web_application/real_estate/views.py
+++ real_estate_web_application/real_estate/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from real_estate_web_application.real_estate.forms import LocationForm
-from real_estate_web_application.real_estate.models import Location
+from real_estate_web_application.real_estate.models import Location, Properties
 
 
 class CreateLocationView(CreateView):
@@ -10,4 +10,11 @@ class CreateLocationView(CreateView):
     form_class = LocationForm
     template_name = 'real-estate/city.html'
     success_url = reverse_lazy('home')
+
+
+class PropertyListView(ListView):
+    model = Properties
+    context_object_name = 'properties'
+    template_name = 'real-estate/properties.html'
+    paginate_by = 2
 
