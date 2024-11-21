c6cb22ab7e6bd310232f20df1b65e8ccc9d06d44
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-21 23:50:39 +0200

    feat: Finished properties page

--- real_estate_web_application/real_estate/choices.py
+++ real_estate_web_application/real_estate/choices.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class TypeChoice(models.TextChoices):
-    ONE_BEDROOM = 'One Bedroom', 'One Bedroom'
-    TWO_BEDROOM = 'Two Bedroom', 'Two Bedroom'
-    THREE_BEDROOM = 'Three Bedroom', 'Three Bedroom'
-    FOUR_BEDROOM = 'Four Bedroom', 'Four Bedroom'
+    ONE_BEDROOM_APARTMENT = 'One-Bedroom Apartment', 'One-Bedroom Apartment'
+    TWO_BEDROOM_APARTMENT = 'Two-Bedroom Apartment', 'Two-Bedroom Apartment'
+    THREE_BEDROOM_APARTMENT = 'Three-Bedroom Apartment', 'Three-Bedroom Apartment'
+    FOUR_BEDROOM_APARTMENT = 'Four-Bedroom Apartment', 'Four-Bedroom Apartment'
     MAISONETTE = 'Maisonette', 'Maisonette'
     BUSINESS_PROPERTY = 'Business Property', 'Business Property'
     HOUSE = 'House', 'House'
