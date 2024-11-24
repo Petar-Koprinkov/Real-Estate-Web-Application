c0d219980441192c57bd0bf1a3780a4044c4d12c
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-24 16:50:52 +0200

    fix: Fixed CSS of the buttons

---
+++ TODO.py
@@ -0,0 +1,8 @@
+# TODO: TO CRATE ANOTHER MODEL, TO BE THE FIFTH MODEL!!!!!! MAY BE LIKE MODEL - IF DONT HAVE IDEAS
+
+# TODO: To add data of the comments
+# TODO: To create comment app
+# TODO: To create page for investor where he can view statistics
+# TODO: To make session for every user to have favourite property listings
+# TODO: To add permissions
+# TODO: To modify admin panel
