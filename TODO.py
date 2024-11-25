d7a231d5b877cfa6d4e6561828078df1c729a2d7
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-25 13:43:17 +0200

    feat: Added favourite button

--- TODO.py
+++ TODO.py
@@ -1,5 +1,3 @@
-# TODO: TO CRATE ANOTHER MODEL, TO BE THE FIFTH MODEL!!!!!! MAY BE LIKE MODEL - IF DONT HAVE IDEAS - Parking slot
-
 # TODO: To make session for every user to have favourite property listings
 # TODO: To add permissions
 # TODO: To modify admin panel
