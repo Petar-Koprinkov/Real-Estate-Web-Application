381b274e17ef2c3701d24f37eadddb63083ad5a1
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-12-03 15:33:32 +0200

    feat: Added Django Admin Panel to comment model

--- real_estate_web_application/common/admin.py
+++ real_estate_web_application/common/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from real_estate_web_application.common.models import CommentModel
 
-# Register your models here.
+
+@admin.register(CommentModel)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'date', 'property', 'user')
+    list_filter = ('property', 'user')
+    date_hierarchy = 'date'
+    search_fields = ('user',)
+    search_help_text = 'Search comment by user.'
+    ordering = ('pk',)
+
+
+
