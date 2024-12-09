daae579ef74ea9f378e795ec72e976d7023a648f
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-12-09 10:09:21 +0200

    feat: Changed DEBUG settings

--- real_estate_web_application/urls.py
+++ real_estate_web_application/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('properties/', include('real_estate_web_application.properties_api.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
