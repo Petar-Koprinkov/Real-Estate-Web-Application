2e4c6242a25922798576bd49a56b9260ed999fb3
Petar Koprinkov <petur_koprinkov@abv.bg>
2025-01-14 16:08:40 +0200

    feat: Login REST

--- real_estate_web_application/settings.py
+++ real_estate_web_application/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
                      'rest_framework',
                      'drf_spectacular',
+                     'rest_framework.authtoken',
 
                  ] + PROJECT_APPS
 
